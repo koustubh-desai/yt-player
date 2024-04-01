7bf45eb4862b2335c7964eda5d54001e7a605116
Koustubh Desai <koustubh.desai@publicissapient.com>
2024-04-01 18:13:22 +0530

    feat(setup): initial file setup

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/git',
+      {
+        "path": "@semantic-release/exec",
+        "cmd": "npm run build"
+      },
+      {
+        "path": "@semantic-release/git",
+        "assets": ["package.json", "dist/**/*", "CHANGELOG.md"]
+      },
+      {
+        "path": "@semantic-release/exec",
+        "cmd": "npm publish --registry <YOUR_ARTIFACTORY_REGISTRY>"
+      }
+    ],
+  };
+  
