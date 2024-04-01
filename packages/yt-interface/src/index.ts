83f4fddb5f3b5d6ae162ac32da7055749d17c583
Koustubh Desai <koustubh.aravind.desai@gmail.com>
2024-04-01 19:31:18 +0530

    feat(versions): updated to latest versions of dev dependencies

---
+++ packages/yt-interface/src/index.ts
@@ -0,0 +1,14 @@
+// packages/yt-interface/src/MyClass.ts
+
+export class MyClass {
+    private message: string;
+  
+    constructor(message: string) {
+      this.message = message;
+    }
+  
+    showMessage() {
+      console.log(this.message);
+    }
+  }
+  
