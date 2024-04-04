9f1918cf174ffbccbb4ba40f06463c915b27920e
Koustubh Desai <koustubh.aravind.desai@gmail.com>
2024-04-04 16:36:22 +0530

    feat(publish 2):

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,7 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+// import {Header} from 'header';
+// import {Footer} from 'footer';
+import {Header} from '../../../header/src/Header';
+import {Footer} from '../../../footer/src/Footer';
 
 export default function Index() {
     return (
