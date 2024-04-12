6dea610d0f5016283c8d5c18c3d5105c34526d3f
Koustubh Desai <koustubh.desai@publicissapient.com>
2024-04-12 19:17:21 +0530

    feat: footer 16 beatles

--- packages/footerbykd12/src/Footer.tsx
+++ packages/footerbykd12/src/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
     return (
         <footer>
-            Footer 15 - Comfortably Numb
+            Footer 16 - Beatles - How deep is your love.
         </footer>
     );
 }
